307779b1555aa0e7efe1e3c79f60883d0e8291fc
Stéphane Campinas <stephane.campinas@deri.org>
2014-08-15 13:47:21 +0100

    Added test for object path

--- sparql/sparql_test.go
+++ sparql/sparql_test.go
@@ -12,6 +12,15 @@ func parse(t *testing.T, query string) *Sparql {
     return s
 }
 
+func TestMultiObjects(t *testing.T) {
+    parse(t, `
+        SELECT *
+        WHERE {
+            ?s <aaa> ?o1, ?o2
+        }
+        `)
+}
+
 func TestEscapedQuotes(t *testing.T) {
     parse(t, `
         SELECT *
